74032143b0380376faf15675266ce0f09b48ebd7
aluto <aluto@softserveinc.com>
2022-12-22 17:07:31 +0100

    bugfix: remove extra TelegrafModule init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { TelegrafModule } from 'nestjs-telegraf';
 
 import { AppController } from './app.controller';
 import { BotModule } from './bot/bot.module';
@@ -11,9 +10,6 @@ import appConfig from './config/app.config';
     ConfigModule.forRoot({
       load: [appConfig],
       isGlobal: true,
-    }),
-    TelegrafModule.forRoot({
-      token: process.env.TELEGRAM_BOT_TOKEN,
     }),
     BotModule,
   ],
